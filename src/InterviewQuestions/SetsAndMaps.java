a55ad57b956639507c52722bc9c65ce5d065dea4
Serdar <serdar2225@gmail.com>
2020-08-11 02:46:22 -0400

    unique elements

--- src/InterviewQuestions/SetsAndMaps.java
+++ src/InterviewQuestions/SetsAndMaps.java
@@ -1,6 +1,9 @@
 package InterviewQuestions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class SetsAndMaps {
     public static void main(String[] args) {
@@ -13,5 +16,21 @@ public class SetsAndMaps {
         for (Integer map1:map.keySet()) {
             System.out.println(map1);
         }
+
+        System.out.println("000000000000000000");
+
+        //find the unique element in the arraylist
+
+        List<String> marketList = new ArrayList<>();
+        marketList.add("apple");
+        marketList.add("orange");
+        marketList.add("apple");
+        marketList.add("apple");
+
+        System.out.println(marketList);
+
+        HashSet<String> uniqueFruit = new HashSet<>(marketList);
+
+        System.out.println(uniqueFruit);
     }
 }
