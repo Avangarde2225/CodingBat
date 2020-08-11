b48d60ac7ca4a70d9afc275eb627de12fbb53fea
Serdar <serdar2225@gmail.com>
2020-08-11 02:00:03 -0400

    finding 100

---
+++ src/InterviewQuestions/sumOfTwoNumbers.java
@@ -0,0 +1,21 @@
+package InterviewQuestions;
+
+public class sumOfTwoNumbers {
+    public static void main(String[] args) {
+        //sum of two numbers that give 100 in total
+
+        int[] arr = {2, 5, 40, 70, 95, 98};
+
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == 100) {
+                    System.out.println(arr[i] +" "+ arr[j]);
+                }
+
+            }
+
+        }
+    }
+}
