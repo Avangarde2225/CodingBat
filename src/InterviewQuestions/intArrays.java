903faa33afd2d8e14748606aed25986e51927502
Serdar <serdar2225@gmail.com>
2020-08-11 00:58:15 -0400

    int array sum odd and even

--- src/InterviewQuestions/intArrays.java
+++ src/InterviewQuestions/intArrays.java
@@ -9,5 +9,18 @@ public class intArrays {
             sum+=arr[i];
         }
         System.out.println(sum);
+        System.out.println("-------------------------");
+//sum if odd subtract if even
+        int[] arr2 = {2,6,8,7,5,2};
+        int result =0;
+        for (int i = 0; i < arr2.length ; i++) {
+            if(arr2[i]%2==1){
+                result += arr2[i];
+            } else{
+                result -= arr2[i];
+            }
+
+        }
+        System.out.println(result);
     }
 }
