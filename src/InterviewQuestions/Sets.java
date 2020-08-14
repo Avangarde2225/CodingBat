c490d45a0f01b0f0e546606d5c2fe3d68a2177e8
Serdar <serdar2225@gmail.com>
2020-08-14 07:53:34 -0400

    sets

---
+++ src/InterviewQuestions/Sets.java
@@ -0,0 +1,9 @@
+package InterviewQuestions;
+
+public class Sets {
+    /*
+    Hashset does not allow duplicates
+    does not follow insertion order except Linkedhashset
+    does allow null values except treeset
+     */
+}
