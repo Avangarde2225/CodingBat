1a691df020d1758b6d5a75ef23260ff4cf2882db
Serdar <serdar2225@gmail.com>
2020-08-23 23:37:25 -0400

    complete method

--- src/interviewQuestionsContinued/thirdLargestArray.java
+++ src/interviewQuestionsContinued/thirdLargestArray.java
@@ -2,6 +2,38 @@ package interviewQuestionsContinued;
 
 public class thirdLargestArray {
     public static void main(String[] args) {
+        int arr[] = {12, 13, 1, 10, 34, 16};
+        int n = arr.length;
+        thirdLargest(arr, n);
 
     }
+
+    static void thirdLargest(int arr[], int arr_size) {
+
+        // There should beatleast three elements
+
+        if (arr_size < 3) {
+            System.out.println(" Invalid Input ");
+        }
+
+        int first = arr[0];
+        int secondLargest= Integer.MIN_VALUE;
+        int thirdLargest = Integer.MIN_VALUE;
+
+
+        for (int i = 1;i < arr_size ; i++)
+            if (arr[i] > first)
+                first = arr[i];
+
+        for (int i = 0; i < arr_size ; i++)
+            if (arr[i] > secondLargest && arr[i] < first)
+                secondLargest = arr[i];
+
+        for (int i = 0; i < arr_size ; i++)
+            if (arr[i] > thirdLargest && arr[i] < secondLargest)
+                thirdLargest = arr[i];
+
+        System.out.printf("The third Largest " + "element is %d\n", thirdLargest);
+    }
 }
+
