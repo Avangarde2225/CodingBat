d96140963705cf1581ee10b638c3f43d59b9af71
Serdar <serdar2225@gmail.com>
2021-04-26 22:44:23 -0400

    letter count

---
+++ src/LeetCode/LetterCount.java
@@ -0,0 +1,34 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class LetterCount {
+    static void charCount(String str){
+
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        char [] newChar = str.toCharArray();
+
+        for (char c : newChar) {
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)+1);
+            }
+            else{
+                map.put(c,1);
+            }
+        }
+
+        for(Map.Entry entry : map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String s = "Babalamamama";
+        charCount(s.toLowerCase(Locale.ROOT));
+    }
+}
